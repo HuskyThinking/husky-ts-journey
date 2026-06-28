c87bcb42f854d405346bb4e59ede2137c1addefb
BeiYue <beitianmingyue@gmail.com>
2026-06-28 16:24:55 +0800

    add react router

--- alex-course-2026/advanced/section-react-router/src/App.jsx
+++ alex-course-2026/advanced/section-react-router/src/App.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet, useNavigate } from 'react-router';
+import { NavLink, Outlet, useLocation, useNavigate } from 'react-router';
 import About from './components/About';
 import Home from './components/Home';
 import { use, useState } from 'react';
@@ -6,12 +6,22 @@ import { use, useState } from 'react';
 function App() {
 
   const navigate = useNavigate();
+  const location = useLocation();
 
+  function handleNavigate() {
+    if (location.pathname === '/') {
+      navigate('/about');
+      return;
+    }
+      navigate('/');
+  }
+  
   return (
     <>
-      <button type='button' onClick={() => navigate('/')}>Go Home</button>
+      <button type='button' onClick={handleNavigate}>Toggle</button>
+      {/* <button type='button' onClick={() => navigate('/')}>Go Home</button>
       <br />
-      <button type='button' onClick={() => navigate('/about')}>Go About</button>
+      <button type='button' onClick={() => navigate('/about')}>Go About</button> */}
       {/* <NavLink to="/">Home</NavLink>
       <br />
       <NavLink to="/about">About</NavLink> */}
